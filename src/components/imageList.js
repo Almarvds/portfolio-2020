279f28f8a431b215533b06de068e0d6c8d75a999
Almarvds <a.r.v.d.stappen@student.tue.nl>
2020-06-17 20:18:36 +0200

    swapped out images for size

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,6 +1,6 @@
 
 const imageList = [
-   process.env.PUBLIC_URL+'images/bubbles/Brabant.jpeg',
+   process.env.PUBLIC_URL+'images/bubbles/Brabant.jpg',
    process.env.PUBLIC_URL+'images/bubbles/bracelets2.jpg',
    process.env.PUBLIC_URL+'images/bubbles/research.png',
    process.env.PUBLIC_URL+'images/bubbles/monDemo.png',
@@ -10,7 +10,7 @@ const imageList = [
   'https://image.flaticon.com/icons/svg/3066/3066586.svg',
    process.env.PUBLIC_URL+'images/bubbles/motion.jpeg',
    process.env.PUBLIC_URL+'images/bubbles/mathbuilder.jpg',
-   process.env.PUBLIC_URL+'images/bubbles/swim.JPG',
+   process.env.PUBLIC_URL+'images/bubbles/IMG_5896.png',
    process.env.PUBLIC_URL+'images/bubbles/froggybig.png',
    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAGF-l79Q_Ay96nqlJ0s079f94RVqwQneyQz-pycRGg%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1',
    process.env.PUBLIC_URL+'images/bubbles/logo.png',
