217e1dacf0ee7afa8e7754661cc0c2dcd45d0d27
Almarvds <a.r.v.d.stappen@student.tue.nl>
2023-07-09 14:13:48 +0200

    Update title_subtext.js

--- src/components/title_subtext.js
+++ src/components/title_subtext.js
@@ -10,7 +10,7 @@ export const titles = [
 export const subtexts  = [
   'I have some experience in every facet of digital products.',
   'Passionate about user oriented AI and automated futures.',
-  'AI product Designer',
+  'Technical product manager',
   'Proud to be an industrial designer.',
   'Always looking for the next big thing.'
 ];
